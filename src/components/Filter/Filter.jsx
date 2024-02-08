f698a2d14f3c71c8ad33e43d332063adf2612a95
Valerii Pometun <vmpometun@gmail.com>
2024-02-08 10:51:29 +0200

    update

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -5,20 +5,18 @@ export class Filter extends Component {
   render() {
     const { setFilter } = this.props;
     return (
-      <form className={style.form}>
-        <label className={style.label}>
-          Find contacts by name
-          <input
-            className={style.input}
-            type="text"
-            name="name"
-            onChange={e => setFilter(e.currentTarget.value)}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-      </form>
+      <label className={style.label}>
+        Find contacts by name
+        <input
+          className={style.input}
+          type="text"
+          name="name"
+          onChange={e => setFilter(e.currentTarget.value)}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
     );
   }
 }
